81b36d8792bb13d93f4bdb718300a61cb2d13130
Dennis Schaeppi <dennisschaeppi@outlook.com>
2017-12-18 20:48:39 +0100

    UPDATE purchase added

--- src/app/updatepurchase.php
+++ src/app/updatepurchase.php
@@ -1,3 +1,13 @@
 <?php
-  echo $_GET['id'];
+   header("Access-Control-Allow-Origin: *");
+   include("db.php");
+
+   $id = $_REQUEST['bestellnr'];
+   $cmt = $_REQUEST['com'];
+   $anz = $_REQUEST['an'];
+
+   $stmt = $odbc->prepare("CALL updatepurchases(".$id.", '".$cmt."', ".$anz.")");
+   $stmt->execute();
+
+   echo "Änderungen vorgenommen!";
 ?>
